c6df99788dea05a9b18c64775396c4f67c22d76e
Alberto Villa <a.villa.diez@gmail.com>
2012-01-08 23:31:44 +0100

    Implemented read of config file

---
+++ aparse.rb
@@ -0,0 +1,38 @@
+#!/usr/bin/ruby
+
+class Applicant
+
+  def initialize config_file
+    if File.exist?(config_file)
+      lines = IO.readlines(config_file)
+      lines.each do |line|
+	    parse_line(line)  	
+	  end	
+    else
+      puts "Config file " + config_file + " not found!"
+	end
+
+  end
+
+  def parse_line line    
+    @name = parse_value(line,"name:") if line.start_with?("name:")
+    @email = parse_value(line,"email:") if line.start_with?("email:")
+    @about = parse_value(line,"about:") if line.start_with?("about:")
+    @urls = parse_value(line,"urls:") if line.start_with?("urls:")   	  
+  end
+
+  def parse_value(line, field)  	  
+  	return line.gsub(field,"").lstrip
+  end
+
+  def print
+  	puts @name
+  	puts @email
+  	puts @about
+  	puts @urls
+  end
+end
+
+
+applicant = Applicant.new "aparse.cfg"
+applicant.print
